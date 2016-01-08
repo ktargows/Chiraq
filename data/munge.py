495231da13516bcbf7a1af7f1802ffce56734927
Ryan Larson <ryan-p-larson@uiowa.edu>
2016-01-08 13:40:01 -0600

    Data init

---
+++ data/munge.py
@@ -0,0 +1,20 @@
+import csv
+
+data = []
+
+chicago = open("homicides.csv")
+for row in csv.DictReader(chicago):
+    origin = "chicago"
+    date = row["Date"][:10]
+    data.append({"origin":origin, "date":date})
+
+iraq = open("soldiers.csv")
+for row in csv.DictReader(iraq):
+    origin = "iraq"
+    date = row["Date"]
+    data.append({"origin":origin, "date":date})
+
+with open("formatted.csv", "w") as outfile:
+    writer = csv.DictWriter(outfile, fieldnames = ["date", "origin"])
+    writer.writeheader()
+    writer.writerows(data)
